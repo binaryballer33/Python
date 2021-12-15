c3d638d1a16b117132cce1afffc81b297f626196
binaryballer33 <53889633+binaryballer33@users.noreply.github.com>
2021-12-14 20:33:21 -0600

    Add files via upload
    Easy: https://leetcode.com/problems/palindrome-number/

---
+++ leetcode/isPalindrome.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        string_number = str(x)
+        number_reversed = ''
+        last_index = len(string_number) - 1
+
+        for each_number in string_number:
+            number_reversed += string_number[last_index]
+            last_index -= 1
+
+        if string_number == number_reversed:
+            return True
+        else:
+            return False
+git
