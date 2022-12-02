f348522e6d790e25b512b941e9394a694af00eb5
Shaquille Mandy <shaqmandy@gmail.com>
2022-12-01 18:08:00 -0800

    added avg_count and file writer

--- Misc/Rubixs_Cube_Solve_Times.py
+++ Misc/Rubixs_Cube_Solve_Times.py
@@ -1,3 +1,4 @@
+#!/usr/local/bin/python3
 """
 Shaquille's
 Rubix's Cube Statisics
@@ -5,48 +6,88 @@ Rubix's Cube Statisics
 """
 import datetime
 
+# get's the avearge time of the last n(number) of solves that you specify
+def last_n_solves_average(solve_times, n, rubix_cube_stats_report):
+    # n = 3 if you want your average from the last 3
+    # n = 5 if you want your average from the last 5
+    # n = 10 if you want your average from the last 10
+    sum = 0
+
+    if len(solve_times) >= n:
+        for index in range(len(solve_times) - 1, len(solve_times) - n - 1, -1):
+            sum += solve_times[index]
+    else:
+        print(f"\t\tYou have not solved the cube {n} times yet")
+        rubix_cube_stats_report.write(f"\t\tYou have not solved the cube {n} times yet\n")
+        
+        return 0
+    
+    print(f"\t\tYour average solve time for the last {n} solves is: {sum / n}") 
+    rubix_cube_stats_report.write(f"\t\tYour average solve time for the last {n} solves is: {sum / n}\n") 
+    
+    return sum / n
 
 def rubix_cube_stats(solve_times, date=datetime.datetime.today()):
     import statistics
     """
     Expects a list of rubix cube solve times for the parameter solve_times
         -Prints out todays date and time. the sum, avg, and amount of solves in your list of solve_times
-
-
     """
-    print("=" * 100)
-    print("Rubix Cube Stats:")
-    print(f'\t\tToday is {date} yyyy-mm-dd: hh:mm:sec:ms')
+    with open('rubix_cube_stats.txt', 'a', encoding='UTF-8') as rubix_cube_stats_report:
+        print("=" * 100)
+        print("Rubix Cube Stats:")
+        print(f'\t\tToday is {date} yyyy-mm-dd: hh:mm:sec:ms')
+        rubix_cube_stats_report.write("=" * 100)
+        rubix_cube_stats_report.write("\n")
+        rubix_cube_stats_report.write("Rubix Cube Stats:\n")
+        rubix_cube_stats_report.write(f"\t\tToday is {date}\n")
 
-    # Generates the Number for the amount of times you solved the rubix cube
-    amount_of_solves = 0
-    for each_solve in solve_times:
-        amount_of_solves = amount_of_solves + 1
-    print(f'\t\tThis information is based on {amount_of_solves} solves:')
+        # Generates the Number for the amount of times you solved the rubix cube
+        amount_of_solves = len(solve_times)
+        print(f'\t\tThis information is based on {amount_of_solves} solves:')
+        rubix_cube_stats_report.write(f"\t\tThis information is based on {amount_of_solves} solves:\n")
 
-    # Generates the Sum for you
-    sum = 0
-    for number in solve_times:
-        sum = sum + number
+        # Generates the Sum for you
+        sum = 0
+        for number in solve_times:
+            sum = sum + number
 
-    print(f'\t\tYour Average time is: {sum / amount_of_solves} seconds' )
-    print(f'\t\tYou solve the rubix\'s cube in {statistics.mode(solve_times)} seconds the most')
-    print("\t\tThe Sum of all your solves is: " + str(sum))
+        print(f'\t\tYour Average time is: {sum / amount_of_solves} seconds' )
+        print(f'\t\tYou solve the rubix\'s cube in {statistics.mode(solve_times)} seconds the most')
+        print("\t\tThe Sum of all your solves is: " + str(sum))
+        rubix_cube_stats_report.write(f'\t\tYour Average time is: {sum / amount_of_solves} seconds\n')
+        rubix_cube_stats_report.write(f'\t\tYou solve the rubix\'s cube in {statistics.mode(solve_times)} seconds the most\n')
+        rubix_cube_stats_report.write("\t\tThe Sum of all your solves is: " + str(sum) + "\n")
 
-    print("_" * 100)
+        # get the averages
+        print()
+        rubix_cube_stats_report.write("\n")
+        last_n_solves_average(solve_times, 3, rubix_cube_stats_report)
+        last_n_solves_average(solve_times, 5, rubix_cube_stats_report)
+        last_n_solves_average(solve_times, 10, rubix_cube_stats_report)
+        last_n_solves_average(solve_times, 20, rubix_cube_stats_report)
+        last_n_solves_average(solve_times, 25, rubix_cube_stats_report)
+        print()
+        rubix_cube_stats_report.write("\n")
 
-    # Prints out the solve and how many times you solved it in that amount of time
-    # Only prints out the solve if it's in the list, if you never solved it in 10 seconds 10 won't be printed out
-    # Example: You solved the rubix cube in 16 seconds 2 times
-    starter_count = 5
-    for each_rubix_completion in range(5, 61):
-        if each_rubix_completion in solve_times:
-            print(f'\t\tYou solved the rubix cube in {each_rubix_completion} seconds {solve_times.count(starter_count)} times out of {amount_of_solves}: {(solve_times.count(starter_count) / amount_of_solves) * 100 }%')
-            starter_count += 1
-        else:
-            starter_count += 1
-    print("=" * 100)
-    print(), print(), print()
+        print("_" * 100)
+        rubix_cube_stats_report.write("_" * 100 + "\n")
+
+        # Prints out the solve and how many times you solved it in that amount of time
+        # Only prints out the solve if it's in the list, if you never solved it in 10 seconds 10 won't be printed out
+        # Example: You solved the rubix cube in 16 seconds 2 times
+        starter_count = 5
+        for each_rubix_completion in range(5, 61):
+            if each_rubix_completion in solve_times:
+                print(f'\t\tYou solved the rubix cube in {each_rubix_completion} seconds {solve_times.count(starter_count)} times out of {amount_of_solves}: {(solve_times.count(starter_count) / amount_of_solves) * 100 }%')
+                rubix_cube_stats_report.write(f'\t\tYou solved the rubix cube in {each_rubix_completion} seconds {solve_times.count(starter_count)} times out of {amount_of_solves}: {(solve_times.count(starter_count) / amount_of_solves) * 100 }%\n')
+                starter_count += 1
+            else:
+                starter_count += 1
+
+        print("=" * 100)
+        rubix_cube_stats_report.write("=" * 100 + "\n\n\n\n\n")
+        print(), print(), print()
 
 
 def random_list_generator(): # generates a random list
@@ -58,8 +99,10 @@ def random_list_generator(): # generates a random list
     return random_list
 
 
-rubix_cube_stats(random_list_generator())
+# rubix_cube_stats(random_list_generator())
 
-solves = [19, 16, 24, 18, 19, 20, 21, 24, 20, 17, 16, 20, 19, 23, 19, 22, 17, 16, 17, 20, 19, 18, 21, 17, 23, 17, 20, 17, 22, 19]
-rubix_cube_stats(solves)
+# solves = [19, 16, 24, 18, 19, 20, 21, 24, 20, 17, 16, 20, 19, 23, 19, 22, 17, 16, 17, 20, 19, 18, 21, 17, 23, 17, 20, 17, 22, 19]
+# rubix_cube_stats(solves, 'March 30th, 2020)
+
+rubix_cube_stats([12, 18, 14, 19, 16, 18, 18, 14, 16, 19, 14, 15, 15, 14, 17, 13, 19, 16, 16, 15, 14, 18, 16, 15, 17], "Thrusday December 1st, 2022")
 
